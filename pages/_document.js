c25acb0d34929cca76a1fa37d65f66689934f142
Rakhsanth <35163960+Rakhsanth@users.noreply.github.com>
2021-06-12 23:31:39 +0530

    Added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,8 @@ export default class MyDocument extends Document {
                         name="theme-color"
                         content={theme.palette.secondary.main}
                     />
+                    {/* favicon icon */}
+                    <link rel="shortcut icon" href="/favicon.ico" />
                     <link
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
